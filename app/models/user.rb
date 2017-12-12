20822c3226ba3fe5524ce9aa7c1829537800447c
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-12-12 23:32:59 +0900

    Create pc view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  has_many :ideas
 end
