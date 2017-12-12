79c1ccad9c050db11f5cb0e5c10a57f6ae5d0989
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-12-12 22:12:35 +0900

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121090258) do
+ActiveRecord::Schema.define(version: 20171212130836) do
+
+  create_table "ideas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title",                    null: false
+    t.text     "body",       limit: 65535, null: false
+    t.integer  "user_id",                  null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["title"], name: "index_ideas_on_title", using: :btree
+    t.index ["user_id"], name: "index_ideas_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -30,4 +40,5 @@ ActiveRecord::Schema.define(version: 20171121090258) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "ideas", "users"
 end
