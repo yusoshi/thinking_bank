d3929b363a8857fdd5ea4c51dc6dc3a02f57e25f
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-12-24 18:30:42 +0900

    Delete idea by asynchronous communication

--- app/assets/javascripts/delete-idea.js
+++ app/assets/javascripts/delete-idea.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.content__main__memos-area__memo-area__edit-and-delete__delete').on('click', function(e) {
+  $(document).on('click', '.content__main__memos-area__memo-area__edit-and-delete__delete', function(e) {
     e.preventDefault();
 
     // DOMで、ideaをリアルタイムで削除
@@ -7,7 +7,8 @@ $(function() {
     $('.content__main__memos-area__memo-area[data-id=' + idea.id +']').remove();
     }
     
-    var id = $('.content__main__memos-area__memo-area').data('id');
+    var id = $(this).parent().data('id');
+
     alert('このアイデアを削除します。よろしいですか？');
 
 
