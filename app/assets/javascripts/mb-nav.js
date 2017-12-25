3abb67588cea3c968a98a25c35eee5f958555fa7
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-12-25 20:39:15 +0900

    Modify header 4

--- app/assets/javascripts/mb-nav.js
+++ app/assets/javascripts/mb-nav.js
@@ -1,7 +1,11 @@
 $(function() {
     $(".toggle-area").on("click", function(e) {
         e.preventDefault();
-        var $header = $("#top-head")
-        $header.toggleClass("open");
+        var $header = $("#top-head");
+        if ($('div').hasClass('second-header')) {
+          $header.toggleClass("idea-index-open");
+        } else {
+          $header.toggleClass("open");
+        }
     });
 });
