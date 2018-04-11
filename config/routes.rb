9f64bd876c40d126fa9a83e0adf915047ddf078b
yusoshi <nsymgnk0405@yahoo.co.jp>
2018-04-11 23:46:02 +0900

    forgot password page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations",
-    passwords: "users/password"
+    passwords: "users/passwords"
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'ideas#index'
